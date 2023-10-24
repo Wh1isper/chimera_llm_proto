4cc39c8863c98a0549c81eb8feb2ae60319b40ec
Wh1isper <9573586@qq.com>
2023-10-24 05:25:02 +0000

    Init proto as llama

--- generate_proto.sh
+++ generate_proto.sh
@@ -2,5 +2,5 @@
 set -e
 
 pip install "grpcio-tools>=1.15.0"
-python -m grpc_tools.protoc -I./proto --python_out=./chimera_llm_proto --grpc_python_out=./chimera_llm_proto ./proto/chimera_llm.proto
+python -m grpc_tools.protoc -I./proto --pyi_out=./chimera_llm_proto --python_out=./chimera_llm_proto --grpc_python_out=./chimera_llm_proto ./proto/chimera_llm.proto
 2to3 -n -w ./chimera_llm_proto/*
