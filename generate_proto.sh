6020f46121c243c6308c562a29273033e450efab
Wh1isper <9573586@qq.com>
2023-10-25 02:38:44 +0000

    Add more spec

--- generate_proto.sh
+++ generate_proto.sh
@@ -3,4 +3,4 @@ set -e
 
 pip install "grpcio-tools>=1.15.0"
 python -m grpc_tools.protoc -I./proto --pyi_out=./chimera_llm_proto --python_out=./chimera_llm_proto --grpc_python_out=./chimera_llm_proto ./proto/chimera_llm.proto
-2to3 -n -w ./chimera_llm_proto/*
+2to3 -n -w ./chimera_llm_proto/*_pb2*
