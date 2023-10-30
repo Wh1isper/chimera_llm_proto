1ac5f8d732317cde1ffb7abbade694aca49d33a1
Wh1isper <9573586@qq.com>
2023-10-30 02:12:00 +0000

    Add pre-commit hook

--- chimera_llm_proto/chimera_llm_pb2.pyi
+++ chimera_llm_proto/chimera_llm_pb2.pyi
@@ -2,7 +2,13 @@ from google.protobuf.internal import containers as _containers
 from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+from typing import (
+    ClassVar as _ClassVar,
+    Iterable as _Iterable,
+    Mapping as _Mapping,
+    Optional as _Optional,
+    Union as _Union,
+)
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
@@ -27,6 +33,7 @@ class Role(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
     SYSTEM: _ClassVar[Role]
     USER: _ClassVar[Role]
     ASSISTANT: _ClassVar[Role]
+
 MODEL_TYPE_UNKNOWN: ModelTag
 GENERAL: ModelTag
 CHAT: ModelTag
@@ -59,7 +66,14 @@ class AvaliableModel(_message.Message):
     model_name: str
     size: str
     model_description: str
-    def __init__(self, model_tagas: _Optional[_Iterable[_Union[ModelTag, str]]] = ..., model_id: _Optional[int] = ..., model_name: _Optional[str] = ..., size: _Optional[str] = ..., model_description: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        model_tagas: _Optional[_Iterable[_Union[ModelTag, str]]] = ...,
+        model_id: _Optional[int] = ...,
+        model_name: _Optional[str] = ...,
+        size: _Optional[str] = ...,
+        model_description: _Optional[str] = ...,
+    ) -> None: ...
 
 class InspectResponse(_message.Message):
     __slots__ = ["avaliable_models", "current_status", "current_model"]
@@ -69,7 +83,12 @@ class InspectResponse(_message.Message):
     avaliable_models: _containers.RepeatedCompositeFieldContainer[AvaliableModel]
     current_status: Status
     current_model: AvaliableModel
-    def __init__(self, avaliable_models: _Optional[_Iterable[_Union[AvaliableModel, _Mapping]]] = ..., current_status: _Optional[_Union[Status, str]] = ..., current_model: _Optional[_Union[AvaliableModel, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        avaliable_models: _Optional[_Iterable[_Union[AvaliableModel, _Mapping]]] = ...,
+        current_status: _Optional[_Union[Status, str]] = ...,
+        current_model: _Optional[_Union[AvaliableModel, _Mapping]] = ...,
+    ) -> None: ...
 
 class LoadModelRequest(_message.Message):
     __slots__ = ["model_id", "json_model_param"]
@@ -77,7 +96,9 @@ class LoadModelRequest(_message.Message):
     JSON_MODEL_PARAM_FIELD_NUMBER: _ClassVar[int]
     model_id: int
     json_model_param: str
-    def __init__(self, model_id: _Optional[int] = ..., json_model_param: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self, model_id: _Optional[int] = ..., json_model_param: _Optional[str] = ...
+    ) -> None: ...
 
 class LoadModelResponse(_message.Message):
     __slots__ = ["current_status", "current_model"]
@@ -85,7 +106,11 @@ class LoadModelResponse(_message.Message):
     CURRENT_MODEL_FIELD_NUMBER: _ClassVar[int]
     current_status: Status
     current_model: AvaliableModel
-    def __init__(self, current_status: _Optional[_Union[Status, str]] = ..., current_model: _Optional[_Union[AvaliableModel, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        current_status: _Optional[_Union[Status, str]] = ...,
+        current_model: _Optional[_Union[AvaliableModel, _Mapping]] = ...,
+    ) -> None: ...
 
 class InferenceArgs(_message.Message):
     __slots__ = ["temperature", "top_p", "max_gen_len", "repetition_penalty", "json_extra_args"]
@@ -99,7 +124,14 @@ class InferenceArgs(_message.Message):
     max_gen_len: int
     repetition_penalty: int
     json_extra_args: str
-    def __init__(self, temperature: _Optional[float] = ..., top_p: _Optional[float] = ..., max_gen_len: _Optional[int] = ..., repetition_penalty: _Optional[int] = ..., json_extra_args: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        temperature: _Optional[float] = ...,
+        top_p: _Optional[float] = ...,
+        max_gen_len: _Optional[int] = ...,
+        repetition_penalty: _Optional[int] = ...,
+        json_extra_args: _Optional[str] = ...,
+    ) -> None: ...
 
 class CompletionRequest(_message.Message):
     __slots__ = ["request_id", "prompt", "inference_args"]
@@ -109,7 +141,12 @@ class CompletionRequest(_message.Message):
     request_id: str
     prompt: str
     inference_args: InferenceArgs
-    def __init__(self, request_id: _Optional[str] = ..., prompt: _Optional[str] = ..., inference_args: _Optional[_Union[InferenceArgs, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        request_id: _Optional[str] = ...,
+        prompt: _Optional[str] = ...,
+        inference_args: _Optional[_Union[InferenceArgs, _Mapping]] = ...,
+    ) -> None: ...
 
 class CompletionPrediction(_message.Message):
     __slots__ = ["request_id", "response_id", "generation"]
@@ -119,7 +156,12 @@ class CompletionPrediction(_message.Message):
     request_id: str
     response_id: str
     generation: str
-    def __init__(self, request_id: _Optional[str] = ..., response_id: _Optional[str] = ..., generation: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self,
+        request_id: _Optional[str] = ...,
+        response_id: _Optional[str] = ...,
+        generation: _Optional[str] = ...,
+    ) -> None: ...
 
 class ChatMessage(_message.Message):
     __slots__ = ["role", "content"]
@@ -127,7 +169,9 @@ class ChatMessage(_message.Message):
     CONTENT_FIELD_NUMBER: _ClassVar[int]
     role: Role
     content: str
-    def __init__(self, role: _Optional[_Union[Role, str]] = ..., content: _Optional[str] = ...) -> None: ...
+    def __init__(
+        self, role: _Optional[_Union[Role, str]] = ..., content: _Optional[str] = ...
+    ) -> None: ...
 
 class ChatRequest(_message.Message):
     __slots__ = ["request_id", "messages", "inference_args"]
@@ -137,7 +181,12 @@ class ChatRequest(_message.Message):
     request_id: str
     messages: _containers.RepeatedCompositeFieldContainer[ChatMessage]
     inference_args: InferenceArgs
-    def __init__(self, request_id: _Optional[str] = ..., messages: _Optional[_Iterable[_Union[ChatMessage, _Mapping]]] = ..., inference_args: _Optional[_Union[InferenceArgs, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        request_id: _Optional[str] = ...,
+        messages: _Optional[_Iterable[_Union[ChatMessage, _Mapping]]] = ...,
+        inference_args: _Optional[_Union[InferenceArgs, _Mapping]] = ...,
+    ) -> None: ...
 
 class ChatPrediction(_message.Message):
     __slots__ = ["request_id", "response_id", "message"]
@@ -147,4 +196,9 @@ class ChatPrediction(_message.Message):
     request_id: str
     response_id: str
     message: ChatMessage
-    def __init__(self, request_id: _Optional[str] = ..., response_id: _Optional[str] = ..., message: _Optional[_Union[ChatMessage, _Mapping]] = ...) -> None: ...
+    def __init__(
+        self,
+        request_id: _Optional[str] = ...,
+        response_id: _Optional[str] = ...,
+        message: _Optional[_Union[ChatMessage, _Mapping]] = ...,
+    ) -> None: ...
